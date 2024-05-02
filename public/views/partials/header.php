2cce935af1720b77677a85765cebc823f0513250
OwenR21 <130512414+OwenR21@users.noreply.github.com>
2024-05-01 22:17:25 -0500

    removed login functionality

--- public/views/partials/header.php
+++ public/views/partials/header.php
@@ -15,8 +15,8 @@
             <li><a href="#services">Services</a></li>
             <li><a href="#about">About Us</a></li>
             <li><a href="#contact">Contact</a></li>
-            <li><a href="/login">Login</a></li>
-            <li><a href="/invoice">Invoices</a></li>
+            <!-- <li><a href="/login">Login</a></li>-->
+            <li><a href="/invoice">Invoices</a></li> 
         </ul>
     </nav>
 </header>
